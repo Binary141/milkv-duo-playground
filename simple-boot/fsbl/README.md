cf4fcbe06dadf1320317523b185ba145c8a9bcc5
Brendan Davidson <binary141@gmail.com>
2024-12-08 16:01:06 -0700

    Migrate to using a shared fsbl folder for all dependant projects

--- simple-boot/fsbl/README.md
+++
@@ -1,3 +0,0 @@
-Cloned and modified code from:
-
-https://github.com/gtxzsxxk/milkv-duo-firmware-fsbl
--- simple-boot/fsbl/plat/cv180x/include/ddr/phy_pll_init.h
+++
@@ -1,6 +0,0 @@
-#ifndef __PHY_PLL_INIT_H__
-#define __PHY_PLL_INIT_H__
-
-void pll_init(void);
-
-#endif /* __PHY_PLL_INIT_H__ */
