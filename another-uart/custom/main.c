0b3f69b45a29f08fced13df39ca6d9842eb4ca13
Brendan Davidson <binary141@gmail.com>
2025-04-19 20:12:01 -0600

    Get the uart 4 working on 57.6k baud

--- another-uart/custom/main.c
+++ another-uart/custom/main.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include "uart.h"
 
 __attribute__ ((aligned (16))) char stack0[4096 * 1];
@@ -5,40 +6,33 @@ __attribute__ ((aligned (16))) char stack0[4096 * 1];
 void uartinit(void);
 
 #define UART0 0x04140000L
-#define UART4 0x04160000L
+
+#define UART4 0x041C0000L
 
 #define CLOCKGENBASEADDR 0x03002000L
 
 #define Reg(reg) ((volatile unsigned char *)(UART4 + (reg)))
 
-#define ClkReg(reg) ((volatile unsigned char *)(CLOCKGENBASEADDR + (reg)))
-#define ReadClkReg(reg) (*(ClkReg(reg)))
-#define WriteClkReg(reg, v) (*(ClkReg(reg)) = (v))
-
 #define WriteReg(reg, v) (*(Reg(reg)) = (v))
+#define ReadReg(reg) (*(Reg(reg)))
 
 #define UART0Reg(reg) ((volatile unsigned char *)(UART0 + (reg)))
 #define WriteUART0Reg(reg, v) (*(UART0Reg(reg)) = (v))
 
-#define LCR_BAUD_LATCH (1<<7) // special mode to set baud rate
+// pinmux address for the pins that can use uart4
+#define REG_IOCTRL_SD1_GPIO1   (*(volatile uint32_t *)0x03001084L)  // For UART4_TX
+#define REG_IOCTRL_SD1_GPIO0   (*(volatile uint32_t *)0x03001088L)  // For UART4_RX
 
-#define RHR 0                 // receive holding register (for input bytes)
-#define THR 0                 // transmit holding register (for output bytes)
-#define IER 0x000                 // interrupt enable register
-#define IER_RX_ENABLE (1<<0)
-#define IER_TX_ENABLE (1<<1)
-#define FCR 2                 // FIFO control register
-#define FCR_FIFO_ENABLE (1<<0)
-#define FCR_FIFO_CLEAR (3<<1) // clear the content of the two FIFOs
-#define ISR 2                 // interrupt status register
-#define LCR 3                 // line control register
-#define LCR_EIGHT_BITS (3<<0)
-#define LCR_BAUD_LATCH (1<<7) // special mode to set baud rate
-#define LSR 5                 // line status register
-#define LSR_RX_READY (1<<0)   // input is waiting to be read from RHR
-#define LSR_TX_IDLE (1<<5)    // THR can accept another character to send
+#define REG_UART4_MUX_SEL (*(volatile uint32_t *)0x0300101E4)
 
-#define ReadReg(reg) (*(Reg(reg)))
+void 
+set_uart4_pins_sd1_gpio(void) 
+{
+    REG_UART4_MUX_SEL = 0x4; // make sure the uart4 pins are connected to the uart4 device
+
+    REG_IOCTRL_SD1_GPIO1 = 0x1;  // Set to UART4_TX
+    REG_IOCTRL_SD1_GPIO0 = 0x1;  // Set to UART4_RX
+}
 
 void
 uartputc_sync(int c)
@@ -47,25 +41,39 @@ uartputc_sync(int c)
   // while((ReadReg(LSR) & LSR_TX_IDLE) == 0)
   //   ;
   WriteReg(0x000, c);
-  // WriteUART0Reg(0x000, c);
+
+  // write to both uarts for now
+  WriteUART0Reg(0x000, c);
 }
 
+#define ClkReg32(reg) ((volatile uint32_t *)(CLOCKGENBASEADDR + (reg)))
+#define ReadClkReg(reg) (*(ClkReg32(reg)))
+#define WriteClkReg(reg, v) (*(ClkReg32(reg)) = (v))
+
 void initclock() {
-    // WriteReg(0x004, (1<<22));
-    // WriteReg(0x004, (1<<23));
+    uint32_t v = ReadClkReg(0x004);  // Read CLK_EN_1
 
-    int current_value = ReadClkReg(0x004);
-
-    current_value |= (1<<18);  // Set bit 22 to 1
-    current_value |= (1<<19);  // Set bit 22 to 1
-                               //
-    WriteClkReg(0x004, current_value);  // Write it back
+    v |= (1 << 22);  // Enable clk_uart4
+    v |= (1 << 23);  // Enable clk_apb_uart4
+                     //
+    WriteClkReg(0x004, v);  // Write it back
 }
 
 int main() {
+    set_uart4_pins_sd1_gpio();
+
+    // small sleep
+    for (volatile int d = 0; d < 10000; d++);
+
     initclock();
 
+    // small sleep
+    for (volatile int d = 0; d < 10000; d++);
+
     uartinit();
+
+    // small sleep
+    for (volatile int d = 0; d < 10000; d++);
 
     int threshold = 0x1000000;
 
@@ -74,7 +82,7 @@ int main() {
     int i = 0;
 
     while (1) {
-        uartputc_sync('v');
+        uartputc_sync('o');
 
         if (i != threshold) {
             i += 1;
@@ -105,11 +113,6 @@ int main() {
 
     return 0;
 }
-    // disable interrupts.
-  // WriteReg(IER, 0);
-
-  // WriteReg(0x00c, LCR_BAUD_LATCH);
-
 
 // init steps
 // 1. Write 1 to LCR [7]. Enable and configure Divisor Latch Access
@@ -120,6 +123,13 @@ int main() {
 // 5. Configure FCR and set corresponding transmit and receive FIFO threshold.
 // 6. Turn off ETBEI/ERBFI in IER
 
+#define LCR_BAUD_LATCH (1<<7) // special mode to set baud rate
+#define LCR 3                 // line control register
+#define IER_RX_ENABLE (1<<0)
+#define IER_TX_ENABLE (1<<1)
+#define FCR_FIFO_CLEAR (3<<1) // clear the content of the two FIFOs
+#define LCR_EIGHT_BITS (3<<0)
+
 void
 uartinit(void)
 {
@@ -127,16 +137,13 @@ uartinit(void)
   WriteReg(0x00c, LCR_BAUD_LATCH);
   WriteReg(LCR, LCR_BAUD_LATCH);
 
-  // LSB for baud rate of 38.4K.
-  WriteReg(0x000, 0x03);
+  // LSB for baud rate of 57.6k
+  WriteReg(0x000, 0x1B);
 
-  // MSB for baud rate of 38.4K.
+  // MSB for baud rate of 57.6k
   WriteReg(0x004, 0x00);
 
-  // leave set-baud mode
-  WriteReg(0x00c, (0<<7));
-
-  // and set word length to 8 bits, no parity.
+  // leave set-baud mode and set word length to 8 bits, no parity.
   WriteReg(0x00c, LCR_EIGHT_BITS);
 
   // reset and enable FIFOs.
