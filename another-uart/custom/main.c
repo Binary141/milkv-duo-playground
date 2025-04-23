241ed57d02b5d8933dc0aa16204e136ec66ed9a1
Brendan Davidson <binary141@gmail.com>
2025-04-23 15:56:52 -0600

    Format and comment out the led blink in main

--- another-uart/custom/main.c
+++ another-uart/custom/main.c
@@ -3,43 +3,45 @@
 __attribute__ ((aligned (16))) char stack0[4096 * 1];
 
 int main() {
-    uartinit();
+  uartinit();
 
-    int threshold = 0x1000000;
+  int threshold = 0x1000000;
 
-    // state of the led
-    int toggle = 0;
-    int i = 0;
+  // state of the led
+  int toggle = 0;
+  int i = 0;
 
-    while (1) {
-        print("h");
+  while (1) {
+    print("yz");
+    for (volatile int i = 0; i < 1000000; i++);  // Slow down output
 
-        if (i != threshold) {
-            i += 1;
-            continue;
-        }
+    // if (i != threshold) {
+    //   i += 1;
+    //   continue;
+    // }
 
-        if (toggle == 1) {
-            __asm__(
-                "li t0, 0x03022000\n\t"
-                "li t1, 1<<24\n\t"
-                "sw t1, (t0)\n\t"
-            );
+    // if (toggle == 1) {
+    //   __asm__(
+    //       "li t0, 0x03022000\n\t"
+    //       "li t1, 1<<24\n\t"
+    //       "sw t1, (t0)\n\t"
+    //   );
 
-            // next state to go to
-            toggle = 0;
-        } else {
-            __asm__(
-                "li t0, 0x03022000\n\t"
-                "li t1, 0<<24\n\t"
-                "sw t1, (t0)\n\t"
-            );
+    //     // next state to go to
+    //   toggle = 0;
+    // } else {
+    //   __asm__(
+    //       "li t0, 0x03022000\n\t"
+    //       "li t1, 0<<24\n\t"
+    //       "sw t1, (t0)\n\t"
+    //   );
 
-            // next state to go to
-            toggle = 1;
-        }
-        i = 0;
-    }
+    //   // next state to go to
+    //   toggle = 1;
+    // }
 
-    return 0;
+    // i = 0;
+  }
+
+  return 0;
 }
